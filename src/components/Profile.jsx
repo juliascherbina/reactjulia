c861bec17b976f23e57e3811afd4e13bd4082906
Julia <juliascherbina15@gmail.com>
2022-03-21 15:30:45 +0200

    init

---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+const Profile = () => {
+    return <div className='content'> <div>
+        <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' />
+    </div>
+        <div>
+            ava + description
+        </div>
+        <div> My posts
+            <div>
+                New Post
+            </div>
+            <div>
+                <div> Post1</div>
+                <div> post2</div>
+            </div>
+        </div>
+    </div>
+}
+export default Profile;
